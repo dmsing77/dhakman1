6842429e3af55a2d0e384664df30e1781f91964c
dmsing77 <35193844+dmsing77@users.noreply.github.com>
2018-01-11 09:00:00 +0545

    Add files via upload

---
+++ WAP to print your name asking input from user..cpp
@@ -0,0 +1,15 @@
+/*Author: Dhakman Sing Sunuwar
+Program: WAP to print your name asking input from user.
+Lab No.: 1
+Date:
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char name;
+	printf("Enter your name");
+	scanf("%s",&name);
+	printf("The name is %s",name);
+	getch();
+}
